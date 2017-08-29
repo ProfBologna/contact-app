854a18a029529426f57492df49ceaf925c1961d4
ProfBologna <sam.aporia@gmail.com>
2017-08-29 17:27:29 -0500

    Added new contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,7 +2,7 @@ class ContactsController < ApplicationController
 
   def first_contact_action
     @title = 
-    @contacts = Contact.find(4)
+    @contacts = Contact.find(5)
     render 'contact_page.html.erb'
   end
 end
